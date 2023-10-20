07878881812016959774ebd6ede1dc299fe1df03
botkesaksham <sa9607@srmist.edu.in>
2023-10-21 01:56:56 +0530

    frontend changes

--- smartplaydapp/src/components/Valorant.jsx
+++ smartplaydapp/src/components/Valorant.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
 import img1 from '../assets/gun1.png'
 import img2 from '../assets/gun2.png'
 import img3 from '../assets/gun3.png'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
+const image = [
+  { id: 1, src: img1 },
+  { id: 2, src: img2 },
+  { id: 3, src: img3 },
+];
 
 export default function Valorant() {
   return (
     <div>
-        <h1 className='text-center font-bold text-3xl text-white p-5'>Showing Assets for <br/> Valorant</h1>
-        <div className='flex items-center justify-center mx-auto'>
-            <Link to="spec"><img src={img1} alt="" /></Link>
-            <Link to="spec"><img src={img2} alt="" /></Link>
-            <Link to="spec"><img src={img3} alt="" /></Link>
-        </div>
+      <h1 className='text-center font-bold text-3xl text-white p-5'>Showing Assets for Valorant</h1>
+      <div className='flex items-center justify-center mx-auto'>
+        {image.map((image) => (
+          <Link to="spec" key={image.id}>
+            <img className="transform transition duration-1000 hover:scale-110" src={image.src} alt="" />
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
